df1aeb8479bec7e3d46cf6a1532b6d2b52420662
Lesly <lnombertoc@gmail.com>
2021-08-23 04:23:48 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import lettersJson from './board.json';
+import { useState } from "react";
 
 function App() {
+  const defaultLetters = lettersJson.board;
+  const [newLetters, setLetters] = useState([])
+  // const [defaultLettersCopy, setDefaultLetterCopy] = useState([
+  //                                                                         { letter: newLetters, clicked: false },
+  //                                                                         { letter: 'A', clicked: false}
+  //                                                                       ]);
+
+  const clickable = (event) => {
+    setLetters([...newLetters, event.target.innerHTML])
+  }
+
+  const empty = () => {
+    setLetters([])
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <div className="empty">
+          <span className="word">clear word</span>
+          <button onClick={empty} className="icon-deleted">X</button>
+        </div>
+        <ul className="main">
+          {
+            defaultLetters.map(item => (
+              <button key={item} onClick={clickable} data-mssg={item} className="button-letter">{ item }</button>)
+            )
+          }
+        </ul>
+        <div className="result">{newLetters.map(letter => (<span key={letter}>{letter}</span>))}</div>
+      </div>
     </div>
   );
 }
